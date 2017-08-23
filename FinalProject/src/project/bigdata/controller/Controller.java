ed06b637cc89d28d8145d4f112df08347a5201f5
ctyvx784 <ctyvx784@gmail.com>
2017-08-24 03:08:27 +0900

    selectService에서 생성된 list를 받아서 json객체 배열로 생성한뒤 내보냄

--- FinalProject/src/project/bigdata/controller/Controller.java
+++ FinalProject/src/project/bigdata/controller/Controller.java
@@ -1,8 +1,11 @@
 package project.bigdata.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
@@ -14,88 +17,128 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.jasper.tagplugins.jstl.core.Out;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import project.bigdata.dto.ResultDTO;
 import project.bigdata.service.NextPage;
 import project.bigdata.service.UserService;
 
 /**
  * Servlet implementation class Controller
  */
-@WebServlet(value="*.project",
-initParams = {
-		@WebInitParam(name = "url",
-					value = "/project/bigdata/util/util.properties")
-		})
+@WebServlet(value = "*.project", initParams = {
+		@WebInitParam(name = "url", value = "/project/bigdata/util/util.properties") })
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
 	private Map<String, UserService> map = new HashMap<>();
-	
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Controller() {
-        super();
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	public Controller() {
+		super();
+	}
+
+	/**
+	 * @return
+	 * @return
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String url = request.getRequestURI();
-		int cmdIdx = url.lastIndexOf("/")+1;
+		int cmdIdx = url.lastIndexOf("/") + 1;
 		String cmd = url.substring(cmdIdx);
-		
+
 		UserService service = map.get(cmd);
 		System.out.println(cmd);
 		NextPage nextPage = service.execute(request, response);
-		
-		if(nextPage == null) {
+
+		if (nextPage == null) {
 			System.out.println("Return Null. Change it");
-		}
-		else {
-			if(nextPage.isRedirect()) {
+
+			PrintWriter out = response.getWriter();
+			String listJson = "";
+			String temp = "";
+
+			// System.out.println(request.getAttribute("list"));
+
+			// 리스트를 불러옴
+			List<ResultDTO> list = (List<ResultDTO>) request.getAttribute("list");
+			// System.out.println(list);
+
+			// 불러온 데이터 리스트를 Json객체 형식으로 구현
+			for (int i = 0; i < list.size(); i++) {
+				temp += "{\"trdar_cd\":" + list.get(i).getTrdar_cd() + ",\"trdar_cd_nm\":" + "\""
+						+ list.get(i).getTrdar_cd_nm() + "\"" + ",\"danger\":" + list.get(i).getDanger()
+						+ ",\"sales_degree\":" + list.get(i).getSales_degree();
+
+				if (i == list.size() - 1) {
+					temp += "}";
+				} else {
+					temp += "},";
+				}
+			}
+
+			listJson = "[" + temp + "]";
+			// Json객체 리스트 확인
+			System.out.println(listJson);
+			System.out.println(listJson.length());
+			// ajax로 내보냄
+			URLEncoder.encode(listJson, "UTF-8");//한글 깨짐 방지를 위해 인코딩하기
+			out.print(listJson.toString());
+			out.flush();
+			out.close();
+
+		} else {
+			if (nextPage.isRedirect()) {
 				System.out.println("go NextPage");
 				response.sendRedirect(nextPage.getPageName());
-			}
-			else {
+			} else {
 				System.out.println("foward");
-				request.getRequestDispatcher(nextPage.getPageName()).forward(request,response);
+				request.getRequestDispatcher(nextPage.getPageName()).forward(request, response);
 			}
 		}
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
-	
+
 	@Override
 	public void init(ServletConfig config) throws ServletException {
-		
+
 		Properties prop = new Properties();
-		
-		//getClass().getResourceAsStream(config.getInitParameter("url"));
-		
+
+		// getClass().getResourceAsStream(config.getInitParameter("url"));
+
 		try {
 			prop.load(getClass().getResourceAsStream(config.getInitParameter("url")));
-			
-			//System.out.println(prop);			
+
+			// System.out.println(prop);
 			Iterator<Object> it = prop.keySet().iterator();
-			
-			while(it.hasNext()) {
+
+			while (it.hasNext()) {
 				String key = (String) it.next();
-				
+
 				Class clazz = Class.forName(prop.getProperty(key));
-				
+
 				UserService service = (UserService) clazz.newInstance();
-				
+
 				map.put(key, service);
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InstantiationException e) {
