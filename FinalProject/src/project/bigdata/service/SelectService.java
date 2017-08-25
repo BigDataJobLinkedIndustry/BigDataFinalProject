cbb17f561ca2b3e032b577cb2c035f18b9d7fafc
obpark1 <obpark1@naver.com>
2017-08-25 18:10:57 +0900

    충돌 실험

--- FinalProject/src/project/bigdata/service/SelectService.java
+++ FinalProject/src/project/bigdata/service/SelectService.java
@@ -81,6 +81,8 @@ public class SelectService implements UserService {
 		out.close();
 		System.out.println("----listJson----");
 		System.out.println(listJson);
+		System.out.println("깃허브 테스트");
+		System.out.println("시작");
 		return null;
 	}
 
