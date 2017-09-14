1c53e13a3754feeed117a73fdff36c79da8b90fb
ctyvx784 <ctyvx784@gmail.com>
2017-09-14 17:13:13 +0900

    htmlText 수정, 페이징 추가

--- FinalProject/WebContent/startbootstrap-modern-business-gh-pages/js/select.js
+++ FinalProject/WebContent/startbootstrap-modern-business-gh-pages/js/select.js
@@ -64,28 +64,89 @@ $(function() {
 				var s_return = $.parseJSON(data);
 				
 				$.each(s_return, function(index, item){
-
 					//결과를 반복해서 출력
-					htmlText += "<tr id = 'html_text'>"
+					htmlText += "<tr class = 'html_text'>"
 						+"<td id = 'trdar_cd_nm'>"+(index+1)+"."+item.trdar_cd_nm+"</td>"
 						+"<td id = 'image_sajin'><img src="+url+item.trdar_cd+".png?raw=true></td>"
-						+"<td>" + "</td>"
 						+"<td id = 'columnchart'><div id='columnchart_values"+index+"'"+"</div></td>"
-						+"<td>" + "</td>"
-						+"<td id = 'chart_div'><div id='chart_div"+index+"'"+"</div></td>"
+						+"<td id = 'chartdiv'><div id='chart_div"+index+"'"+"</div></td>"
 						+"</tr>";
-					
 					showList(item,index);
 				});
 				//결과 리스트 출력
-				$("#trdarList").html(htmlText);	
-				
+				$('#paginated tbody').html(htmlText);
 				//건수 출력
 				$("#totCntSpan").html("(" +s_return.length+ "건)");
-				
 			}
-			
 		});
 		
+		//페이징
+		var $setRows = $('#setRows');
+
+		$setRows.submit(function (e) {
+		    e.preventDefault();
+		    var rowPerPage = $('[name="rowPerPage"]').val() * 1;// 1 을  곱하여 문자열을 숫자형로 변환
+
+		    var zeroWarning = 'Sorry, but we cat\'t display "0" rows page. + \nPlease try again.'
+		    if (!rowPerPage) {
+		        alert(zeroWarning);
+		        return;
+		    }
+		    $('#nav').remove();
+		    var $products = $('#paginated');
+
+		    $products.before('<span id="nav">');
+
+
+		    var $tr = $($products).find('tbody tr');
+		    var rowTotals = $tr.length;
+		    console.log(rowTotals);
+
+		    var pageTotal = Math.ceil(rowTotals/ rowPerPage);
+		    var i = 0;
+
+		    for (; i < pageTotal; i++) {
+		        $('<a href="#"></a>')
+		                .attr('rel', i)
+		                .html(i + 1)
+		                .appendTo('#nav');
+		    }
+
+		    $tr.addClass('off-screen')
+		            .slice(0, rowPerPage)
+		            .removeClass('off-screen');
+
+		    var $pagingLink = $('#nav a');
+		    $pagingLink.on('click', function (evt) {
+		        evt.preventDefault();
+		        var $this = $(this);
+		        if ($this.hasClass('active')) {
+		            return;
+		        }
+		        $pagingLink.removeClass('active');
+		        $this.addClass('active');
+
+		        // 0 => 0(0*4), 4(0*4+4)
+		        // 1 => 4(1*4), 8(1*4+4)
+		        // 2 => 8(2*4), 12(2*4+4)
+		        // 시작 행 = 페이지 번호 * 페이지당 행수
+		        // 끝 행 = 시작 행 + 페이지당 행수
+
+		        var currPage = $this.attr('rel');
+		        var startItem = currPage * rowPerPage;
+		        var endItem = startItem + rowPerPage;
+
+		        $tr.css('opacity', '0.0')
+		                .addClass('off-screen')
+		                .slice(startItem, endItem)
+		                .removeClass('off-screen')
+		                .animate({opacity: 1}, 300);
+
+		    });
+
+		    $pagingLink.filter(':first').addClass('active');
+
+		});
+		$setRows.submit();
 	});
 });
